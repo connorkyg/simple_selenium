86f7ed68883172289448e4281692450dc394cf45
Connor Kwon <connorkyg@gmail.com>
2023-01-25 15:48:54 +0900

    a

--- test.py
+++
@@ -1,9 +0,0 @@
-import simple_selenium
-
-tistory = simple_selenium.browser()
-naver = simple_selenium.browser()
-kakao = simple_selenium.browser()
-
-if __name__ == '__main__':
-    kakao.open("https://kakao.com")
-    kakao.xpath()
